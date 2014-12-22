883285487f9dc42a7a299ef55f10946b2fefa1bd
bupt-steven <654125664@qq.com>
2014-12-22 18:09:18 +0800

    中心界面合板

--- application/controllers/user_information.php
+++ application/controllers/user_information.php
@@ -5,7 +5,7 @@ class User_information extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('login_model');
-		$this->load->model('mission_model');
+		$this->load->model('user_information_model');
 		$this->load->library('session');
 	}
 	public function index(){
@@ -13,11 +13,15 @@ class User_information extends CI_Controller {
 		if ( ! file_exists(APPPATH.'/views/' . 'pub_mission' . '.php')){
 			show_404();
 		}
+		$uid = $this->session->userdata('uid');
 		$data = array(
-			'title' => '个人信息',
-			'uid' => $this->session->userdata('uid')
+			'title' => '个人中心',
+			'login_user' => $this->login_model->get_by_uid($uid),
+			'user_mission' =>  $this->user_information_model->get_user_mission($uid),
+			'user_reply' => $this->user_information_model->get_user_reply($uid)
+			
 			);
-		$data['login_user'] = $this->login_model->get_by_uid($data['uid']);
+		//$data['login_user'] = $this->login_model->get_by_uid($data['uid']);
 	
 		
 			$this->load->view('templates/header', $data);
@@ -25,10 +29,49 @@ class User_information extends CI_Controller {
 			$this->load->view('templates/footer');
 		
 		
-			$uid = $this->session->userdata('uid');
+			//$uid = $this->session->userdata('uid');
 			
 			
 	}
+	/*public function mission(){
+		$this->load->library('session');
+		if ( ! file_exists(APPPATH.'/views/' . 'pub_mission' . '.php')){
+			show_404();
+		}
+		$uid = $this->session->userdata('uid');
+		$data = array(
+			'title' => '个人中心',
+			'user_mission' =>  $this->user_information_model->get_user_mission($uid)
+			);
+			
+		//$data['user_mission'] = $this->user_information_model->get_user_mission($data['uid']);
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('user_information',$data);
+			$this->load->view('templates/footer');
+		
+	}
+	public function reply(){
+		$this->load->library('session');
+		if ( ! file_exists(APPPATH.'/views/' . 'pub_mission' . '.php')){
+			show_404();
+		}
+		$data = array(
+			'title' => '个人中心',
+			'uid' => $this->session->userdata('uid')
+			);
+			echo $data['uid'];
+		$data['user_reply'] = $this->user_information_model->get_user_reply($data['uid']);
+		
+		
+			$this->load->view('templates/header', $data);
+			$this->load->view('user_information',$data);
+			$this->load->view('templates/footer');
+		
+			
+			
+	}*/
+	
 		
 
 	
